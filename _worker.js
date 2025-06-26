81b8f56ea410cb5cc7241b0b7dfcc929900bb293
streetvibe111 <streetvibeapparel05@gmail.com>
2025-06-26 19:12:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,18 +1,13 @@
 export default {
   async fetch(request, env, ctx) {
     try {
-      // Ganti URL Apps Script kau di sini
       const apiUrl = 'https://script.google.com/macros/s/AKfycbx_Ef4iD8aUWqPbTSiHx64v4YyUDWwIZXN3h8V5ILm1/dev';
-
-      // Fetch nombor dari Google Sheet (Apps Script)
       const response = await fetch(apiUrl);
       const phone = (await response.text()).trim();
 
-      // WhatsApp link dengan ayat prefill
       const prefillText = 'hi , salam saya nak tempah baju harga kilang , anggaran kuantiti =';
       const redirectUrl = `https://wa.me/${phone}?text=${encodeURIComponent(prefillText)}`;
 
-      // Redirect user
       return Response.redirect(redirectUrl, 302);
 
     } catch (err) {
@@ -20,3 +15,4 @@ export default {
     }
   },
 };
+
