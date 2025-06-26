07834619a3dc4512ab9003987236613490111c43
streetvibe111 <streetvibeapparel05@gmail.com>
2025-06-26 15:57:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,5 @@
 export default {
   async fetch(request) {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbx_Ef4iD8aUWqPbTSiHx64v4YyUDWwIZXN3h8V5ILm1/dev");
-    const number = await response.text();
-    const redirectUrl = `https://wa.me/${number}?text=Hi%20bang%2C%20saya%20nak%20tempah%20jersey%20harga%20kilang`;
-
-    return Response.redirect(redirectUrl, 302);
+    return Response.redirect("https://wa.me/60134503255?text=Hi", 302);
   }
 }
