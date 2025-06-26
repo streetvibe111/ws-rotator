215bab57f387ca9dc8d5ceb2c933b939b45e2c14
streetvibe111 <streetvibeapparel05@gmail.com>
2025-06-26 21:33:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    const res = await fetch("https://script.google.com/macros/s/AKfycbx_Ef4iD8aUWqPbTSiHx64v4YyUDWwIZXN3h8V5ILm1/dev");
+    const res = await fetch("https://script.google.com/macros/s/AKfycby9Lc_FOVZ21Zr3brki8mxTJqKvjL1QfWZYx8vh6HNSeRarirbuvrpZ3R0MaxHLzNIteQ/exec");
     const json = await res.json();
     const phone = json.phone;
 
