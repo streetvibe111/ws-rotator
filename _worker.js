dd3db82405a04df31c6a7399fc876b373d0e2f73
streetvibe111 <streetvibeapparel05@gmail.com>
2025-06-26 19:26:46 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    const res = await fetch("https://script.google.com/macros/s/AKfycbx_.../exec");
+    const res = await fetch("https://script.google.com/macros/s/AKfycbx_Ef4iD8aUWqPbTSiHx64v4YyUDWwIZXN3h8V5ILm1/dev");
     const json = await res.json();
     const phone = json.phone;
     const msg = "hi, salam saya nak tempah baju harga kilang, anggaran kuantiti =";
