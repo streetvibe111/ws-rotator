a95629b6f3b27c2259296620424242dce881d5cc
streetvibe111 <streetvibeapparel05@gmail.com>
2025-06-26 19:23:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,18 +1,11 @@
 export default {
   async fetch(request, env, ctx) {
-    try {
-      const apiUrl = 'https://script.google.com/macros/s/AKfycbx_Ef4iD8aUWqPbTSiHx64v4YyUDWwIZXN3h8V5ILm1/dev';
-      const response = await fetch(apiUrl);
-      const phone = (await response.text()).trim();
-
-      const prefillText = 'hi , salam saya nak tempah baju harga kilang , anggaran kuantiti =';
-      const redirectUrl = `https://wa.me/${phone}?text=${encodeURIComponent(prefillText)}`;
-
-      return Response.redirect(redirectUrl, 302);
-
-    } catch (err) {
-      return new Response('Error fetching number: ' + err.message, { status: 500 });
-    }
-  },
-};
+    const res = await fetch("https://script.google.com/macros/s/AKfycbx_.../exec");
+    const json = await res.json();
+    const phone = json.phone;
+    const msg = "hi, salam saya nak tempah baju harga kilang, anggaran kuantiti =";
+    const url = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
+    return Response.redirect(url, 302);
+  }
+}
 
