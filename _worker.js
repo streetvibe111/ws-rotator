1f107d987dc82c793072705a43729760ccca3c2e
streetvibe111 <streetvibeapparel05@gmail.com>
2025-06-26 22:31:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,13 +1,13 @@
 export default {
   async fetch(request, env, ctx) {
+    // Ambil nombor telefon dari KV
     const phone = await env.ws_rotator_kv.get("ws_current_phone");
 
-    if (!phone) {
-      return new Response("No phone number found", { status: 404 });
-    }
+    // Teks mesej WhatsApp
+    const msg = "hi , salam saya nak tempah baju harga kilang , anggaran kuantiti = ";
+    const url = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
 
-    const message = encodeURIComponent("hi , salam saya nak tempah baju harga kilang , anggaran kuantiti =");
-    const waURL = `https://wa.me/${phone}?text=${message}`;
-    return Response.redirect(waURL, 301);
-  },
+    // Redirect ke WhatsApp
+    return Response.redirect(url, 302);
+  }
 };
