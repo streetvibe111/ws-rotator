ad7ec50edca4fc6b016daaaa24405661e55c00d1
streetvibe111 <streetvibeapparel05@gmail.com>
2025-06-26 16:01:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,5 +1,9 @@
 export default {
   async fetch(request) {
-    return Response.redirect("https://wa.me/60134503255?text=Hi", 302);
+    const response = await fetch("https://script.google.com/macros/s/AKfycbx_Ef4iD8aUWqPbTSiHx64v4YyUDWwIZXN3h8V5ILm1/dev");
+    const phone = await response.text();
+
+    const url = `https://wa.me/${phone}?text=hi%20%2C%20salam%20saya%20nak%20tempah%20baju%20harga%20kilang%20%2C%20anggaran%20kuantiti%20%3D`;
+    return Response.redirect(url, 302);
   }
 }
